aa5dd8934799e083dc289bb13e45d8f0aa411218
Andy Musey <p.emusey@gmail.com>
2024-12-02 12:06:57 +0000

    lesson completed

--- 06TUT/server.js
+++ 06TUT/server.js
@@ -3,14 +3,25 @@ const app = express() // alternate name, server app
 const path = require('path');
 const cors = require('cors')
 const {logger} = require('./middleware/logEvents')
+const errorHandler = require('./middleware/errorHandler')
 const PORT = process.env.PORT || 3500;  // port created
 
 // custom middleware logger
-const whitelist = ['https://www.yoursite.com']
 app.use(logger)
 
 //Cross Origin Resource Sharing [CORS]
-app.use(cors())
+const whitelist = ['https://www.yoursite.com', 'http://127.0.0.1', 'http://localhost:3500']
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null,true)
+        }else {
+            callback(new Error ('Not allowed by CORS'))
+        }
+    },
+    optionsSuccessStatus:200
+}
+app.use(cors(corsOptions))
 
 // built-in middleware to handle urlencoded data
 //in other words, form data
@@ -60,9 +71,25 @@ const three = (req, res) => {
 app.get('/chain(.html)?', [one, two, three]);
 
 
-app.get('/*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+// app.get('/*', (req, res) => {
+//     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+// })
+
+
+app.all('*', (req, res) => {
+    res.status(404);
+    if(req.accepts('html')){
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    }
+    else if (req.accepts('json')){
+        res.json({ error: "404 Not Found"})
+    } else {
+        res.type('txt').send("404 Not Not Found");
+    }
 })
 
+//app.use is for middleware and does not accept rejex and app.all is more for 
+// routing and would apply to http methods at once
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
